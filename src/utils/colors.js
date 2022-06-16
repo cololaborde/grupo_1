6454944a76912a49ad606dd53bb3dcf36e58e73f
Montes, Matias Ezequiel <mati.montes@hotmail.com>
2022-06-15 22:08:49 -0300

    Update colors.js

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,15 +1,19 @@
+import { useSelector } from "react-redux";
+import { selectHighContrast } from "../store/Home/selectors";
+
+const highContrast = useSelector(selectHighContrast);
 export const COLORS = {
   white: "#f0f0f0",
   primary: "",
   secondary: "",
-  error: "#F56666",
-  link: "#37cae6",
-  text: "#000000",
-  bg_primary: "#d0d0d0",
-  bg_secondary: "#e8e6e6",
-  bg: "#e8e6e6",
-  btn_primary: "#4BCCDB",
-  btn_secondary: "#a0a0a0",
-  btn_error: "#F56666",
-  btn_text: "#FFFFFF",
+  error: highContrast ? "#F56666" : "#F56666",
+  link: highContrast ? "#37cae6" : "#37cae6",
+  text: highContrast ? "#000000" : "#000000",
+  bg_primary: highContrast ? "#d0d0d0" : "#d0d0d0",
+  bg_secondary: highContrast ? "#e8e6e6" : "#e8e6e6",
+  bg: highContrast ? "#e8e6e6" : "#e8e6e6",
+  btn_primary: highContrast ? "#4BCCDB" : "#4BCCDB",
+  btn_secondary: highContrast ? "#a0a0a0" : "#a0a0a0",
+  btn_error: highContrast ? "#F56666" : "#F56666",
+  btn_text: highContrast ? "#FFFFFF" : "#FF0000",
 };
