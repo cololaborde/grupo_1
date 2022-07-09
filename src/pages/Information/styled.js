e8ed0c66883fbb52507971a8e0513e18638e74f7
Montes, Matias Ezequiel <mati.montes@hotmail.com>
2022-07-09 17:06:24 -0300

    changes information content style

--- src/pages/Information/styled.js
+++ src/pages/Information/styled.js
@@ -176,10 +176,43 @@ export const ContentTitle = styled.h2`
   margin: 15px;
 `;
 
-export const Content = styled.p`
-  font-size: ${(props) => props.fontSize || "15px"};
+export const SecondContentTitle = styled.h3`
+  font-size: ${(props) => props.fontSize || "30px"};
   color: ${(props) => props.color || props.theme.text};
   font-weight: bold;
+  font-family: Arial;
+  margin: 15px;
+  @media (max-width: 420px) {
+    margin: 5px;
+  }
+`;
+
+export const ThirdContentTitle = styled.h4`
+  font-size: ${(props) => props.fontSize || "20px"};
+  color: ${(props) => props.color || props.theme.text};
+  font-weight: bold;
+  font-family: Arial;
+  margin: 15px;
+  @media (max-width: 420px) {
+    margin: 5px;
+  }
+`;
+
+export const Content = styled.div``;
+
+export const ContentText = styled.p`
+  font-size: ${(props) => props.fontSize || "15px"};
+  color: ${(props) => props.color || props.theme.text};
+  font-family: Arial;
+  margin: 15px;
+  @media (max-width: 420px) {
+    margin: 5px;
+  }
+`;
+
+export const ContentList = styled.li`
+  font-size: ${(props) => props.fontSize || "15px"};
+  color: ${(props) => props.color || props.theme.text};
   font-family: Arial;
   margin: 15px;
   @media (max-width: 420px) {
