098564b9e1d93e81819b3494878141ac17207a22
cololaborde <cololaborde@gmail.com>
2022-07-11 00:01:14 -0300

    redirect when no data

--- src/pages/DocumentViewer/index.js
+++ src/pages/DocumentViewer/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import BasicDocument from "../../components/PDF";
 import { selectDataArray } from "../../store/DocumentViewer/selectors";
+import { goToInformation } from "../../store/Home/actions";
 
 const DocumentViewer = () => {
   const data = useSelector(selectDataArray);
+  const dispatch = useDispatch();
+  if (!data || data.length == 0) dispatch(goToInformation);
   return (
     <div>
       <BasicDocument data={data} />
